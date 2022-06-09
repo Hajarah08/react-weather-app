1309ee79dda6e26e69b1f2a7825314de546d31fc
Hajarah08 <koikiadetutu@gmail.com>
2022-06-09 13:37:05 +0000

    added the repository link

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    
+    <div>
       <h1>React Weather App</h1>
+
+      <footer>This project was created by Lawal Ajarat and it is <a href="https://github.com/Hajarah08/react-weather-app" target="_blank"> open-source</a></footer>
+      </div>
   );
 }
 
