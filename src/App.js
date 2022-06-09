3c7fa007b0a9c45aceea0043897f8c10d1179c0a
Hajarah08 <koikiadetutu@gmail.com>
2022-06-09 14:18:22 +0000

    added bootstrapp

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <div className="container">
       <h1>React Weather App</h1>
 
       <footer>This project was created by Lawal Ajarat and it is <a href="https://github.com/Hajarah08/react-weather-app" target="_blank"> open-source</a></footer>
+      </div>
       </div>
   );
 }
